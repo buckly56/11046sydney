b06870be6bfbc09c383c9a027bff8c92ad2b8fb4
buckly56 <45410163+buckly56@users.noreply.github.com>
2021-04-27 18:43:15 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,13 @@
+let A = 0
+let S = 0
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        let W = 0
+        A = W + 2
+        basic.showNumber(S)
+    }
+    S += 97
+})
+basic.forever(function () {
+	
+})
